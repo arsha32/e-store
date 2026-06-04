e0c635c86138b18e857e54d0914320def4feceed
arsha32 <114841106+arsha32@users.noreply.github.com>
2026-06-04 22:12:38 +0530

    updated frontend url

--- src/main/java/com/example/ecommerce/security/WebConfig.java
+++ src/main/java/com/example/ecommerce/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:5173/","http://localhost:3000/","https://e-store-front-dx27.vercel.app/")
+        .allowedOrigins("http://localhost:5173/","http://localhost:3000/","https://e-commerce-store-ten-navy.vercel.app/")
         .allowedMethods("GET", "POST","PUT","DELETE","OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
